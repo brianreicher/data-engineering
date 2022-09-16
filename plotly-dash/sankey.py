852210b97a1fe3ea93728f302016970c6e046411
brianreicher <reicher.b@northeastern.edu>
2022-09-16 14:33:42 -0400

    Demo2 and node settings for sankey plot

--- plotly-dash/sankey.py
+++ plotly-dash/sankey.py
@@ -6,12 +6,31 @@ import pandas as pd
 # Browser default for rendering
 pio.renderers.default = "browser"
 
-# Read vertically
-source = [0, 0]
-target = [1, 2]
-value = [2, 1]
 
-link = {'source': source, 'target': target, 'value': value}
-sankey = go.Sankey(link=link)
-fig = go.Figure(sankey)
-fig.show()
+def demo1() -> None:
+    # Read vertically
+    source = [0, 0]
+    target = [1, 2]
+    value = [2, 1]
+
+    link = {'source': source, 'target': target, 'value': value}
+    sankey = go.Sankey(link=link)
+    fig = go.Figure(sankey)
+    fig.show()
+
+
+def demo2() -> None:
+    source = [0, 0, 3, 3]
+    target = [1, 2, 2, 1]
+    value = [1, 2, 3, 4]
+    label = ['A', 'B', 'C', 'D']
+    link = {'source': source, 'target': target, 'value': value}
+    node = {'label': label, 'pad': 15, 'thickness': 35}
+
+    sankey = go.Sankey(link=link, node=node)
+    fig = go.Figure(sankey)
+    fig.show()
+
+
+if __name__ == "__main__":
+    demo2()
