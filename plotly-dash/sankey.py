420787c02c738bc08d1523723bc4d6d4b9d6f0c5
brianreicher <reicher.b@northeastern.edu>
2022-09-16 14:20:52 -0400

    Basic sankey plot

--- plotly-dash/sankey.py
+++ plotly-dash/sankey.py
@@ -1,5 +1,12 @@
-import plotly
+import plotly.graph_objects as go
 import pandas as pd
 
+# Read vertically
+source = [0, 0]
+target = [1, 2]
+value = [2, 1]
 
-print('plotly-test')
+link = {'source': source, 'target': target, 'value': value}
+sankey = go.Sankey(link=link)
+fig = go.Figure(sankey)
+fig.show()
