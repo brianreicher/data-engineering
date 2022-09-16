0265d57fe13d774d3eebb8b5379ab3123f77efb2
brianreicher <reicher.b@northeastern.edu>
2022-09-16 14:47:48 -0400

    Advanced plots

--- plotly-dash/sankey.py
+++ plotly-dash/sankey.py
@@ -32,5 +32,26 @@ def demo2() -> None:
     fig.show()
 
 
+def demo3() -> None:
+    source = [0, 0, 0, 1, 1, 2, 2, 2]
+    target = [3, 4, 5, 3, 5, 3, 4, 5]
+    value = [1, 1, 1, 1, 2, 2, 0.5, 1]
+    label = ['Stomach', 'Lung', 'Brain', 'Gx', 'Gy', 'Gz']
+
+    link_colors = ['lightgrey'] * 4
+    link_colors[1] = '#E18D32'
+    link_colors[2] = 'rgb(38,105,149)'
+    link = {'source': source, 'target': target, 'value': value, 'color': link_colors}
+
+    node_colors = ['mediumslateblue']*3 + ['palegoldenrod']*3
+    node = {'label': label, 'pad': 100, 'thickness': 35, 'line': {'color': 'black',
+                                                                  'width': 2},
+                                                         'color': node_colors}
+
+    sankey = go.Sankey(link=link, node=node)
+    fig = go.Figure(sankey)
+    fig.show()
+
+
 if __name__ == "__main__":
-    demo2()
+    demo3()
