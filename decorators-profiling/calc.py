bd731dcc8904b68ef1b71440f0bc961caa88dda5
brianreicher <reicher.b@northeastern.edu>
2022-09-30 14:17:08 -0400

    OOD updated

--- decorators-profiling/calc.py
+++ decorators-profiling/calc.py
@@ -9,23 +9,40 @@ class Calculator:
 
     def __init__(self, name: str):
         """
-            Constructor
-            :param name:
+            Constructor method
+
+            :param name: str:
                 The name of the calculator instance
         """
-        self.name = name
+        self._name = name
 
-    def add(self, x, y):
-        """Method to perform addition"""
-        print(f'{self.name} is thinking')
+    def add(self, x: int, y: int):
+        """
+            Method to perform addition
+
+            :param x: int
+                First integer to add
+            :param y: int
+                Second integer to add
+        """
+        print(f'{self.get_name()} is thinking . . .')
         return x+y
+
+    def get_name(self) -> str:
+        """
+            Method to get the calculator name
+
+            :return:
+                Calculator name
+        """
+        return self._name
 
 
 def main():
     c1 = Calculator('HP 41CV')
+    print(c1.add(5, 10))
     c2 = Calculator('TI84 CE')
-
-    print(c1.add(5, 10), c2.add(10, 20))
+    print(c2.add(10, 20))
 
 
 if __name__ == '__main__':
