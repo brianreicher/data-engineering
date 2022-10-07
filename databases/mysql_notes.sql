55f71cd3baad728e62df685546b681f916c67158
brianreicher <reicher.b@northeastern.edu>
2022-10-07 13:45:52 -0400

    MySQL notes

--- databases/mysql_notes.sql
+++ databases/mysql_notes.sql
@@ -1,7 +1,7 @@
 -- display different local databases
 show databases;
 
--- create aribitray database
+-- create arbitrary database
 create database foo;
 
 -- use bio database
@@ -16,8 +16,19 @@ FROM GAD;
 select distinct gene_symbol
 from gene;
 
-SELECT phenotype, gene_symbol
+SELECT phenotype, gene_symbol, count(*) as NumPubs
 FROM gad
 WHERE association = 'Y' and phenotype = 'asthma'
 GROUP BY phenotype, gene_symbol
-order by phenotype, gene_symbol;
+HAVING count(*)>=3
+order by NumPubs desc;
+
+SELECT *
+FROM gene
+WHERE gene_symbol = 'APOE';
+
+SELECT *
+FROM go
+WHERE gene_id = (SELECT gene.gene_id
+FROM gene
+WHERE gene_symbol = 'APOE');
